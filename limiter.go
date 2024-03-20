75d7a1ccaa3ce72f35be62d79b1645170a3b1201
ning <chenxiaoning@piesat.cn>
2024-03-20 16:39:55 +0800

    封装copy

--- limiter.go
+++ limiter.go
@@ -4,12 +4,12 @@ import (
 	"io"
 )
 
-func CopyBuffer(w *io.Writer, r io.Reader, speed int64) error {
+func CopyBuffer(w io.Writer, r io.Reader, speed int64) error {
 	// 适当调整buf和rate速率
 	buf := make([]byte, speed)
 	TotalLimit := NewRateLimiter(TransRate(speed), 2)
 	limitReader := NewLimitReaderWithLimiter(TotalLimit, r, false)
-	_, copyErr := io.CopyBuffer(*w, limitReader, buf)
+	_, copyErr := io.CopyBuffer(w, limitReader, buf)
 	if copyErr != nil {
 		return copyErr
 	}
